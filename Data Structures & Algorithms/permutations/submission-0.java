66d9eaf9c29e8d521ce9a09669040d8a50e3d8a3
MogaliLokesh <78530824+MogaliLokesh@users.noreply.github.com>
2026-06-15 19:34:20 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(res,new ArrayList<>(), nums);
+        return res;
+    }
+
+    public void helper(List<List<Integer>>res, List<Integer>temp, int[] nums){
+        if(temp.size()==nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(temp.contains(nums[i])){
+                continue;
+            }
+            temp.add(nums[i]);
+            helper(res,temp, nums);
+            temp.remove(temp.size()-1);
+        }
+    }
+
+
+ 
+}
