2dcaaa2b2efba0b12f764f76dcbb7e9280d84f3c
MogaliLokesh <78530824+MogaliLokesh@users.noreply.github.com>
2026-09-05 14:52:33 +0530

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        return helper(root,0);
+        
+    }
+
+    int helper( TreeNode root, int level){
+        if(root==null) return level;
+
+        return Math.max(helper(root.left,level)+1, helper(root.right,level)+1);
+    }
+}
