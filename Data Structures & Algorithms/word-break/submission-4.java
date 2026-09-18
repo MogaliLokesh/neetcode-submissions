71f47bd3fc087caf64e661ea4bda6beffaef8d6d
MogaliLokesh <78530824+MogaliLokesh@users.noreply.github.com>
2026-09-18 17:02:45 +0530

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int[] track = new int[s.length()+1];
+        return helper(0,s,wordDict,track);
+    }
+
+    boolean helper(int i, String s, List<String> wordDict,int[] track){
+        if(i>=s.length()) return true;
+        if(track[i]==1) return true;
+
+
+
+        for(String word:wordDict){
+            if(i+word.length()<=s.length() && s.substring(i,i+word.length()).equals(word)){
+                // if(helper(i+word.length(),s,wordDict)) return true;
+                if (track[i+word.length()]==0) {
+                    if(helper(i+word.length(),s,wordDict,track)){
+                        track[i+word.length()]=1;
+                        return true;
+                    }
+                    else{
+                        track[i+word.length()]=2;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
